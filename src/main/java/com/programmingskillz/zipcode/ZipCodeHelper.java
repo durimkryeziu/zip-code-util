dc2381861b4da97f05f1e6e7d3c367652758d3f1
Durim Kryeziu <durimkryeziu7@gmail.com>
2017-12-10 09:47:51 +0100

    refactor: Remove unnecessary final

--- src/main/java/com/programmingskillz/zipcode/ZipCodeHelper.java
+++ src/main/java/com/programmingskillz/zipcode/ZipCodeHelper.java
@@ -8,7 +8,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.regex.Pattern;
 
-public final class ZipCodeHelper {
+public class ZipCodeHelper {
 
   /**
    * Don't let anyone instantiate this class.
